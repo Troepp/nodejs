226de29f5f698ae14f5976a4e9741f660f7c29a8
Troepp <103064398+Troepp@users.noreply.github.com>
2024-10-22 10:02:55 +0000

    a

--- bazadanych.js
+++ bazadanych.js
@@ -4,3 +4,16 @@ const db = mysql.createConnection({
     password: 'mariadb',
     database: 'mariadb'
 });
+
+var sql = "CREATE TABLE IF NOT EXISTS users(test int);"
+connection.query(sql);
+
+connection.quer("SHOW TABLES", function(err,rows){
+    if(console.log(err)){
+        console.log(err);
+    }
+    else{
+        console.log(rows);
+    }
+})
+connection.end();
